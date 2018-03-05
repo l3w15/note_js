9100fe2da06ae9e5c6414d96999c610be35b3e3c
carlostrapet <trapet_llamas@hotmail.com>
2018-03-05 17:35:53 +0000

    create list file. added diagram/planning for List

---
+++ src/note-list-model.js
@@ -0,0 +1,4 @@
+// create function that takes argument string
+// and creates new Note passing string as argument
+// so that it's stored in note._text
+// and pushes new Note into List.arrayOfNotes
